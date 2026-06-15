e1932aa923c8a93b3425cfe30a344d937a31fbfb
a-gupta-8 <avibox9511051@gmail.com>
2026-06-14 23:42:09 -0700

    resume page initial implementation

---
+++ proud-proxima/src/components/BackButton.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"; 
+import "../styling/BackButton.css";
+
+export default function BackButton({ page, href }) {
+  const [show, setShow] = useState(false);
+  const [hovering, setHovering] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShow(true);
+    }, 500); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!hovering) return;
+
+    const timer = setTimeout(() => {
+      window.location.href = href;
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [hovering, href]);
+
+  return (
+    <div className={`nav-box ${show ? "show" : ""}`} onMouseEnter={() => show && setHovering(true)} onMouseLeave={() => setHovering(false)}>
+      <span className="nav-text">{page}</span>
+    </div>
+  );
+}
